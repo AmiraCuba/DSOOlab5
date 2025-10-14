ec9b25a2abd7c0da1c0ed3cc33d37c2d78d4fa0a
Alejandro <amendozapan@unsa.edu.pe>
2025-10-14 18:18:51 -0500

    Doctor.java

---
+++ Doctor.java
@@ -0,0 +1,51 @@
+package javaapplication73;
+public class Doctor {
+    
+    private int codigo;
+    private String nombre;
+    private String especialidad;
+    private String horario;
+    
+    public Doctor(int codigo, String nombre, String especialidad, String horario){
+        this.codigo=codigo;
+        this.especialidad=especialidad;
+        this.horario=horario;
+        this.nombre=nombre;
+    }
+    
+    public int getCodigo(){
+        return codigo;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public String getEspecialidad(){
+        return especialidad;
+    }
+    
+    public String getHorario(){
+        return horario;
+    }
+    
+    public void setCodigo(int codigo){
+        this.codigo=codigo;
+    }
+
+    public void setHorario(String horario) {
+        this.horario=horario;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+    
+    public void setEspecialidad(String especialidad){
+        this.especialidad=especialidad;
+    }
+    
+    public String toString(){
+        return "Nombre: "+nombre+"\nEspecialidad: "+especialidad+"\nCodigo: "+codigo+"\nHorario: "+horario;
+    }
+}
